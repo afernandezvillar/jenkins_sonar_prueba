e594196b4f62a9027bdea381715ad1623f85f7e0
U01A507C <afernandezv@caixabanktech.com>
2023-07-09 13:36:01 +0200

    Subida tests

--- test_model.py
+++ test_model.py
@@ -1,4 +1,11 @@
 from model import TabularRegressor
 
+def test_tabular_regressor():
+    regressor = TabularRegressor()
+    x_train = [[1, 2], [3, 4]]
+    y_train = [5, 6]
 
+    regressor.train(x_train, y_train)
+    y_prediction = regressor.predict([[5, 6], [7, 8]])
 
+    assert len(y_prediction) == 2
