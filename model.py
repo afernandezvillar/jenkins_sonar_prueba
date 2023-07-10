14a8ed1216418562229250faac7dea9f2643c06c
U01A507C <afernandezv@caixabanktech.com>
2023-07-10 10:34:58 +0200

    Cambio de codigo

--- model.py
+++ model.py
@@ -1,13 +1,54 @@
-from sklearn.svm import SVR
+
+class CuentaBancaria:
+    def __init__(self, titular, saldo_inicial=0):
+        self.titular = titular
+        self.saldo = saldo_inicial
+
+    def depositar(self, monto):
+        if monto > 0:
+            self.saldo += monto
+            print(f"Depósito realizado. Saldo actual: {self.saldo}")
+        else:
+            print("El monto debe ser mayor que cero.")
+
+    def retirar(self, monto):
+        if monto > 0:
+            if monto > self.saldo:
+                print("Fondos insuficientes.")
+            else:
+                self.saldo -= monto
+                print(f"Retiro realizado. Saldo actual: {self.saldo}")
+        else:
+            print("El monto debe ser mayor que cero.")
+
+    def transferir(self, monto, cuenta_destino):
+        if monto > 0:
+            if monto > self.saldo:
+                print("Fondos insuficientes para realizar la transferencia.")
+            else:
+                self.saldo -= monto
+                cuenta_destino.depositar(monto)
+                print(f"Transferencia realizada. Saldo actual: {self.saldo}")
+        else:
+            print("El monto debe ser mayor que cero.")
+
+    def consultar_saldo(self):
+        print(f"Saldo actual: {self.saldo}")
 
 
-class TabularRegressor:
-    def __init__(self):
-        self.model = None
+# Ejemplo de uso
+cuenta1 = CuentaBancaria("Juan Perez", 1000)
+cuenta2 = CuentaBancaria("Maria Lopez", 500)
 
-    def train(self, x_train, y_train):
-        self.model = SVR()  # Instancia del regresor SVR
-        self.model.fit(x_train, y_train)
+cuenta1.consultar_saldo()
+cuenta2.consultar_saldo()
 
-    def predict(self, x_test):
-        return self.model.predict(x_test)
+cuenta1.depositar(500)
+cuenta1.retirar(200)
+
+cuenta1.consultar_saldo()
+
+cuenta1.transferir(300, cuenta2)
+
+cuenta1.consultar_saldo()
+cuenta2.consultar_saldo()
--- test_model.py
+++ test_model.py
@@ -1,11 +1,61 @@
-from model import TabularRegressor
+from model import CuentaBancaria
+import pytest
 
-def test_tabular_regressor():
-    regressor = TabularRegressor()
-    x_train = [[1, 2], [3, 4]]
-    y_train = [5, 6]
 
-    regressor.train(x_train, y_train)
-    y_prediction = regressor.predict([[5, 6], [7, 8]])
+@pytest.fixture
+def cuentas():
+    cuenta1 = CuentaBancaria("Juan Perez", 1000)
+    cuenta2 = CuentaBancaria("Maria Lopez", 500)
+    return cuenta1, cuenta2
 
-    assert len(y_prediction) == 2
+
+def test_depositar(cuentas):
+    cuenta1, _ = cuentas
+    cuenta1.depositar(500)
+    assert cuenta1.saldo == 1500
+
+
+def test_retirar(cuentas):
+    _, cuenta2 = cuentas
+    cuenta2.retirar(200)
+    assert cuenta2.saldo == 300
+
+
+def test_transferir(cuentas):
+    cuenta1, cuenta2 = cuentas
+    cuenta1.transferir(500, cuenta2)
+    assert cuenta1.saldo == 500
+    assert cuenta2.saldo == 1000
+
+
+def test_retirar_fondos_insuficientes(cuentas):
+    cuenta1, _ = cuentas
+    cuenta1.retirar(2000)
+    assert cuenta1.saldo == 1000
+
+
+def test_transferir_fondos_insuficientes(cuentas):
+    cuenta1, cuenta2 = cuentas
+    cuenta1.transferir(2000, cuenta2)
+    assert cuenta1.saldo == 1000
+    assert cuenta2.saldo == 500
+
+
+def test_depositar_monto_negativo(cuentas):
+    cuenta1, _ = cuentas
+    cuenta1.depositar(-200)
+    assert cuenta1.saldo == 1000
+
+
+def test_retirar_monto_negativo(cuentas):
+    _, cuenta2 = cuentas
+    cuenta2.retirar(-500)
+    assert cuenta2.saldo == 500
+
+
+def test_transferir_monto_negativo(cuentas):
+    cuenta1, cuenta2 = cuentas
+    cuenta1.transferir(-300, cuenta2)
+    assert cuenta1.saldo == 1000
+    assert cuenta2.saldo == 500
+
