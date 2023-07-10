a71a60bb9c6b99d965a7d9ef067e842c96d4e7ed
U01A507C <afernandezv@caixabanktech.com>
2023-07-10 16:37:15 +0200

    Codigo y primeros tests

--- model.py
+++ model.py
@@ -1,3 +1,10 @@
+## Codigo de ejemplo para probar una pipeline de integración de Jenkins con SonarQube
+##
+## Descripcion: La clase CuentaBancaria permite simular la creacion de una cuenta bancaria para una persona determinada
+## con un salario determinado. Cada objeto CuentaBancaria tiene varios métodos.
+##
+## ---------------------------------------------------------------------------------
+
 
 class CuentaBancaria:
     def __init__(self, titular, saldo_inicial=0):
@@ -9,7 +16,7 @@ class CuentaBancaria:
             self.saldo += monto
             print(f"Depósito realizado. Saldo actual: {self.saldo}")
         else:
-            print("El monto debe ser mayor que cero.")
+            print("La cantidad debe ser mayor que cero.")
 
     def retirar(self, monto):
         if monto > 0:
@@ -19,7 +26,7 @@ class CuentaBancaria:
                 self.saldo -= monto
                 print(f"Retiro realizado. Saldo actual: {self.saldo}")
         else:
-            print("El monto debe ser mayor que cero.")
+            print("La cantidad debe ser mayor que cero.")
 
     def transferir(self, monto, cuenta_destino):
         if monto > 0:
@@ -30,15 +37,15 @@ class CuentaBancaria:
                 cuenta_destino.depositar(monto)
                 print(f"Transferencia realizada. Saldo actual: {self.saldo}")
         else:
-            print("El monto debe ser mayor que cero.")
+            print("La cantidad debe ser mayor que cero.")
 
     def consultar_saldo(self):
         print(f"Saldo actual: {self.saldo}")
 
 
 # Ejemplo de uso
-cuenta1 = CuentaBancaria("Juan Perez", 1000)
-cuenta2 = CuentaBancaria("Maria Lopez", 500)
+cuenta1 = CuentaBancaria("Alvaro Cuenta 1", 1000)
+cuenta2 = CuentaBancaria("Alvaro Cuenta 2", 500)
 
 cuenta1.consultar_saldo()
 cuenta2.consultar_saldo()
